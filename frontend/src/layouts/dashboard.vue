e78dccce44fb940cc7d7957f6b1d5e827b48200c
justivome <ivo.i.karaneshev@gmail.com>
2026-03-01 01:35:00 +0200

    fix(frontend): overflow styling problem and make siteheader title dynamic

--- frontend/src/layouts/dashboard.vue
+++ frontend/src/layouts/dashboard.vue
@@ -4,6 +4,8 @@ import { useMeQuery } from '~/api/auth'
 const auth = useAuthStore()
 const { data: user } = useMeQuery()
 
+const route = useRoute()
+
 watch(user, (u) => {
   if (u) {
     auth.user = u
@@ -12,13 +14,13 @@ watch(user, (u) => {
 </script>
 
 <template>
-  <SidebarProvider>
+  <SidebarProvider class="overflow-hidden h-svh">
     <AppSidebar />
-    <SidebarInset>
-      <SiteHeader title="Dashboard" />
+    <main class="flex flex-1 flex-col min-w-0 overflow-y-auto">
+      <SiteHeader :title="route.meta.title as string || 'Dashboard'" />
       <div class="p-4 flex flex-1 flex-col gap-4">
         <RouterView />
       </div>
-    </SidebarInset>
+    </main>
   </SidebarProvider>
 </template>
