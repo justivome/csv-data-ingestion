3096b428d624e6eace68c75805796a2121134830
justivome <ivo.i.karaneshev@gmail.com>
2026-03-01 01:06:12 +0200

    chore(frontend): add select component from shadcn-vue

---
+++ frontend/src/components/ui/select/SelectGroup.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { SelectGroupProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { SelectGroup } from 'reka-ui'
+
+const props = defineProps<SelectGroupProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <SelectGroup :class="cn('p-1 w-full', props.class)" v-bind="delegatedProps">
+    <slot />
+  </SelectGroup>
+</template>
---
+++ frontend/src/components/ui/select/SelectScrollUpButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { SelectScrollUpButtonProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { SelectScrollUpButton, useForwardProps } from 'reka-ui'
+
+const props = defineProps<SelectScrollUpButtonProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <SelectScrollUpButton v-bind="forwardedProps" :class="cn('flex cursor-default items-center justify-center py-1', props.class)">
+    <slot>
+      <Icon name="i-lucide:chevron-up" class="h-4 w-4" />
+    </slot>
+  </SelectScrollUpButton>
+</template>
