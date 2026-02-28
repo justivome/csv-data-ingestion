bf6f73b0437bd55847914896082aeea46fa9d7a4
justivome <ivo.i.karaneshev@gmail.com>
2026-03-01 01:36:43 +0200

    lint: fix

--- frontend/src/components/dashboard/SiteHeader.vue
+++ frontend/src/components/dashboard/SiteHeader.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <header class="px-4 border-b flex shrink-0 gap-2 h-16 items-center">
+  <header class="px-4 border-b bg-background flex shrink-0 gap-2 h-16 items-center top-0 sticky z-10">
     <SidebarTrigger class="-ml-1" />
     <Separator orientation="vertical" class="mr-2 h-4" />
     <h1 class="text-sm font-medium">
